7f6451e4d6e99ffc9b7aa183772c0a20d7edf649
Percy1229 <percy3227@yahoo.co.jp>
2020-01-17 18:44:16 +0900

    add dbfile

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,28 +7,14 @@ use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
 use Validator;
 use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\DB;
 class HelloController extends Controller
 {
   public function index(Request $request) {
-    if($request->hasCookie('msg'))
-    {
-      $msg = 'Cookie: '.$request->cookie('msg');
-    } else {
-      $msg = '*クッキーはありません。';
-    }
-    return view('hello.index', ['msg'=>$msg]);
+    $items = DB::select('select * from people');
+    return view('hello.index', ['items' => $items]);
 }
 
 public function post(Request $request) {
-  $validate_rule = [
-    'msg' => 'required',
-  ];
-  $this->validate($request, $validate_rule);
-  $msg = $request->msg;
-  $response = response()->view('hello.index', [
-    'msg' =>"「${msg}」をクッキーに保存しました。"
-  ]);
-  $response->cookie('msg', $msg, 100);
-  return $response;
 }
 }
